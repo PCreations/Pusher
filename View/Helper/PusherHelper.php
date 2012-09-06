669fb96925f2995aa3d3bdea4bc526c54e86b323
Pierre Criulanscy <pcriulan@gmail.com>
2012-09-06 18:17:00 +0200

    Add simple helper to subscribe on public channel only (for now) and bind event to it

--- View/Helper/PusherHelper.php
+++ View/Helper/PusherHelper.php
@@ -1,17 +1,47 @@
 <?php
 
+App::uses('String', 'Utility');
+
 class PusherHelper extends Helper {
 	
-	public $helpers = array('Html');
+	public $helpers = array('Html', 'Js');
 
 	private $appKey = '';
 
 	public function __construct(View $view, $settings = array()) {
 		parent::__construct($view, $settings);
-		$this->apiKey = Configure::read('Pusher.credentials.appKey');
+		$this->appKey = Configure::read('Pusher.credentials.appKey');
+		$this->Js->buffer('var pusher = new Pusher(\'' . $this->appKey . '\');');
 	}
 
-	
+	public function afterRender($layout) {
+		echo $this->Html->script('http://js.pusher.com/1.12/pusher.min.js', array('inline' => false));
+		echo $this->Js->writeBuffer(array('inline' => false));
+	}
+
+	public function subscribe($channelName, $type = 'public') {
+		$channelName = strtolower($channelName);
+		$channelName = ($type == 'private' || $type == 'presence') ? $type . '-' . $channelName : $channelName;
+		$this->Js->buffer('pusher.subscribe(\'' . $channelName . '\')');
+	}
+
+	public function bindEvent($channelName, $event, $script) {
+		$script = String::insert(
+			$script,
+			array(
+				'message' => "'+data.message+'"
+			)
+		);
+		$this->Js->buffer(
+			$this->getChannel($channelName) . '.bind("' . $event . '", function(data) {
+				' . $script . '
+			});'
+		);
+	}
+
+	private function getChannel($channelName) {
+		return 'pusher.channel(\'' . $channelName . '\')';
+	}
 }
 
 ?>
