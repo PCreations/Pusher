96e79f8a3659b19ba3e5f7d76767b59265598b4a
Pierre Criulanscy <pcriulan@gmail.com>
2012-09-10 14:12:06 +0200

    Update readme

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -6,9 +6,9 @@ Router::parseExtensions('json');
 Configure::write(array(
 	'Pusher' => array(
 		'credentials' => array(
-			'appKey' => 'c9eb8e7d6833194323cc',
-			'appSecret' => 'a4acb3e917e16b69be1e',
-			'appId' => '26484'
+			'appKey' => 'YOUR_APP_KEY',
+			'appSecret' => 'YOUR_APP_SECRET',
+			'appId' => 'YOUR_APP_ID'
 		),
 		'channelAuthEndpoint' => array(
 			'plugin' => 'pusher',
